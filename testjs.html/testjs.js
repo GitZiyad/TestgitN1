ece0ef923f016ff1c2f56ee69525166abf8758a7
GitZiyad <ziyadnuini45@gmail.com>
2023-10-25 14:30:33 +0100

    done

---
+++ testjs.html/testjs.js
@@ -0,0 +1,18 @@
+var number=10
+var number2=20
+console.log(number+number2)
+var hotel ={
+    name :'agdal', numondial : 12, nompersonnel : 150 , totchambre : 300 , parking : false , location : 'marrakech' , recomendation : true
+}
+console.log(hotel)
+if (hotel.numondial <=12){
+    console.log('this hotel is great')
+    recomendation = true
+    
+}else{
+    console.log('we dont recommand it')
+    recomendation =false
+    
+}
+
+console.log('This hotel is classed ',hotel.numondial,'in the world')
